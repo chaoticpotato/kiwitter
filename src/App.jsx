af5f3e527faa5e95f1cf196cba1917ad1153dd01
Ömer Tanrıverdi <omer.f.tanriverdi@gmail.com>
2024-03-11 11:11:58 +0300

    tailwind, routing, basic design added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
+import { Switch, Route, NavLink } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <h1>Selam</h1>;
+  return (
+    <div className="max-w-3xl mx-auto">
+      <header className="flex justify-between items-center">
+        <div className="text-orange-900 text-xl">
+          <NavLink className="p-4" activeClassName="font-bold" to="/">
+            Kiwitter
+          </NavLink>
+        </div>
+        <nav className="flex">
+          <NavLink className="p-4" activeClassName="font-bold" to="/profilim">
+            Profilim
+          </NavLink>
+          <NavLink className="p-4" activeClassName="font-bold" to="/giris-yap">
+            Giriş yap
+          </NavLink>
+        </nav>
+      </header>
+      <main className="p-4 bg-white rounded-lg">
+        <Switch>
+          <Route path="/" exact>
+            <h2>Anasayfa</h2>
+            <p>Burada tüm akış görünecek</p>
+          </Route>
+          <Route path="/profilim">
+            <h2>Profil</h2>
+            <p>Kendi yazdığım twitler görünecek</p>
+          </Route>
+          <Route path="/giris-yap">
+            <h2>Giriş yap</h2>
+          </Route>
+          <Route path="/kayit-ol">
+            <h2>Kayıt ol</h2>
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
